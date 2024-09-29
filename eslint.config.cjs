b9782fc7e437c682e41c8825796201c92239064d
vlad <vnelipovich63@gmail.com>
2024-09-29 14:41:20 +0300

    chore: setup ESLint, Prettier and AirBnB config

---
+++ eslint.config.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'],
+          'internal',
+          ['parent', 'sibling', 'index'],
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
